87fed59d3ac4441e90799bec12fda42d9dc2573b
Darlin Estrada Patino <darlin.estrada456@gmail.com>
2026-03-06 17:32:27 -0500

    feat: validaciones de login e intentos fallidos

--- main.js
+++ main.js
@@ -1,36 +1,102 @@
-const admin = "admin"
-const clave = "1234"
 
-function validarUsuarioYcontraseina(usuario, password) {
-    if (admin === usuario && clave === password) {
-        console.log("Inicio se sesión exitoso");
-        return true
-    } else {
-        console.log("Usuario no valido");
-        return false
+function iniciarData(usuarios) {
+    const identificacion = "0000000000"
+    const correo = ""
+    const admin = "admin"
+    const clave = "1234"
+    const saldo = 0
+    const intentos = 0
+    const adminData = {
+        "identificacion": identificacion,
+        "username": admin,
+        "correo": correo,
+        "clave": clave,
+        "saldo": saldo,
+        "intentos": intentos,
+        "estado": "activo", //activo o bloqueado
+        "movimientos": []
     }
+    usuarios.push(adminData)
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
 }
+
+
+function validarUsuarioYcontraseina(usuario, password){
+    const usuarios = JSON.parse(localStorage.getItem("usuarios"))
+    usuarios.forEach(user => {
+        if (user.username === usuario && user.clave === password) {
+            if (user.estado === "bloqueado") {
+                alert("Usuario bloqueado, por favor contacte al administrador");
+                console.log("Usuario bloqueado, por favor contacte al administrador");
+                return false;
+            }else{
+                alert("Inicio se sesión exitoso");
+                console.log("Inicio se sesión exitoso");
+                return true;
+            }
+        }else {
+            console.log("Usuario o contraseña invalidos");
+            return false
+        }
+    });
+}
+
+function incrementarIntentosFallidos(user) {
+    alert("Le quedan " + (2 - user.intentos) + " intentos");
+    console.log("Le quedan " + (2 - user.intentos) + " intentos");
+    user.intentos++;
+    return user.intentos;
+}
+
 function menu() {
     let opcion = prompt("1.Retirar\n2.Consultar saldo\n3.Consigar\n4.Consultar movimientos\n5.Salir")
     return opcion
 }
 
 function menuInicio() {
-    let inicio = prompt("1.Iniciar\n2.Registrase\n3.Salir")
+    let inicio = prompt("1.Iniciar sesion\n2.Registrase\n3.Salir")
     return inicio
 }
 
-function registrarUsuario(){
-    let usuario = prompt("Por favor cree un nombre de usuario")
-    let contraseña = prompt ("Por favor cree una contraseña")
-    let montoInicial = prompt ("Ahora ingrese su saldo inicial")
-    return [usuario,contraseña,montoInicial]
+function registrarUsuario() {
+    let identificacion = prompt("Por favor ingrese su numero de identificacion")
+    let username = prompt("Por favor cree un nombre de usuario")
+    let correo = prompt("Ahora ingrese su correo electronico")
+    let clave = prompt("Por favor cree una clave de acceso")
+    while (true) {
+        let confirmarClave = prompt("Confirme su clave de acceso")
+        if (confirmarClave === clave) {
+            break
+        } else {
+            console.log("Las claves no coinciden, por favor intente de nuevo")
+        }
+    }
+    while (true) {
+        let montoInicial = prompt("Ahora ingrese su saldo inicial")
+        if (!isNaN(montoInicial) && montoInicial >= 0) {
+            break
+        } else {
+            console.log("El monto inicial no es válido, por favor intente de nuevo")
+        }
+    }
+    return {
+        "identificacion": identificacion,
+        "username": username,
+        "correo": correo,
+        "clave": clave,
+        "saldo": montoInicial,
+        "intentos": 0,
+        "estado": "activo", //activo o bloqueado
+        "movimientos": []
+    }
 }
 
 function main() {
     let usuarios = []
-    let inicio = ""
+    iniciarData(usuarios)
+    let inicio = true
     while (inicio) {
+        inicio = menuInicio()
         switch (inicio) {
             case "1":
                 let usuario = prompt("Por favor ingresa tu nombre de usuario")
@@ -59,17 +125,39 @@ function main() {
 
                     }
 
+                } else {
+                    usuarios.forEach(user => {
+                        if (user.username === usuario) {
+                            if (user.estado === "bloqueado") {
+                                if (incrementarIntentosFallidos(user) >= 3) {
+                                    console.log(user);
+                                    alert("Usuario bloqueado, por favor contacte al administrador");
+                                    console.log("Usuario bloqueado, por favor contacte al administrador");
+                                    user.estado = "bloqueado";
+                                    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+                                }
+                            }else{
+                                alert("Usuario bloqueado, por favor contacte al administrador");
+                                console.log("Usuario bloqueado, por favor contacte al administrador");
+                            }
+
+                        }
+                    });
                 }
 
                 break;
             case "2":
-
+                let nuevoUsuario = registrarUsuario()
+                usuarios.push(nuevoUsuario)
+                localStorage.setItem("usuarios", JSON.stringify(usuarios))
                 break;
             case "3":
-
+                inicio = false
+                console.log("Terminando ejecucion");
                 break;
 
             default:
+                console.log("Valor no valido");
                 break;
         }
 
